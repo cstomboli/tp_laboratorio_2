1b7830835cb2da022ba80692abbf066407e983ca
cstomboli <carostomboli@hotmail.com>
2019-10-06 10:17:52 -0300

    ..

---
+++ TP-02/TP-02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
