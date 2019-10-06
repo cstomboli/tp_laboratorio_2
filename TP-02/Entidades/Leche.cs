237a3865014fb7906bd59afc4a43fdb507123573
cstomboli <carostomboli@hotmail.com>
2019-10-06 11:32:54 -0300

    ..

---
+++ TP-02/Entidades/Leche.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Leche : Producto
+    {
+        public enum ETipo
+        {
+            Entera, Descremada
+        }
+        private ETipo tipo; // ver cuando llama al segundo constructor q pasa con el tipo.
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string codigoDeBarras, ConsoleColor color)
+            : base(marca, codigoDeBarras, color)
+        {
+            this.tipo = ETipo.Entera;
+        }
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string codigoDeBarras, ConsoleColor color, ETipo tipo)
+            : this(marca, codigoDeBarras, color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;                             // this.CantidadCalorias=20; se escribe asi si hay set, pero no hay set!!
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP-02/Entidades/Producto.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#pragma warning disable
+
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Producto no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Producto
+    {
+        public enum EMarca
+        {
+            Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
+        }
+
+        protected EMarca marca;
+        protected string codigoDeBarras;
+        protected ConsoleColor colorPrimarioEmpaque;
+
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de calorias
+        /// </summary>
+        protected abstract short CantidadCalorias
+        {
+            get;
+            //set; lo comento por que en la ppt dice que solo va el get
+        }
+
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        /// <summary>
+        /// La sobrecarga del operador string para usarlo como una
+        /// funcion ToString, para mostar los atributos de un producto.
+        /// </summary>
+        /// <param name="p">Un producto</param>
+        public static explicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("CODIGO DE BARRAS: {0}\r\n", p.codigoDeBarras);
+            sb.AppendFormat("MARCA          : {0}\r\n", p.marca.ToString());
+            sb.AppendFormat("COLOR EMPAQUE  : {0}\r\n", p.colorPrimarioEmpaque.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Compara si dos prodcutos son iguales, si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1">Producto uno</param>
+        /// <param name="v2">Producto dos</param>
+        /// <returns></returns>
+        public static bool operator ==(Producto v1, Producto v2)
+        {
+            return (v1.codigoDeBarras == v2.codigoDeBarras);
+        }
+        /// <summary>
+        /// Compara si dos productos son distintos, si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1">Producto uno</param>
+        /// <param name="v2">Producto dos</param>
+        /// <returns></returns>
+        public static bool operator !=(Producto v1, Producto v2)
+        {
+            return !(v1 == v2);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="codigoDeBarras"></param>
+        /// <param name="colorPrimarioEmpaque"></param>
+        public Producto(EMarca marca, string codigoDeBarras, ConsoleColor colorPrimarioEmpaque)
+        {
+            this.marca = marca;
+            this.codigoDeBarras = codigoDeBarras;
+            this.colorPrimarioEmpaque = colorPrimarioEmpaque;
+        }
+    }
+}
