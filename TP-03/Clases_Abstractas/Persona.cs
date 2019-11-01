6b253ee2f1621989b60983e19473490061293d90
cstomboli <carostomboli@hotmail.com>
2019-11-01 16:47:53 -0300

    Cree 2 clases

---
+++ TP-03/Clases_Abstractas/Persona.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Abstractas
+{
+    public abstract class Persona
+    {
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad; 
+        private string nombre;
+
+        enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+
+        #region Propiedades
+
+        public string Appeldio { get; set; }
+
+        public int DNI { get; set; }
+
+        //public ENacionalidad Nacionalidad { get; set; } FALTA CREAR ENacionalidad
+
+        public string Nombre { get; set; }
+
+        public string stringToDni { get; set; }
+
+        #endregion
+
+        #region Métodos
+
+        public Persona ()
+        {
+
+        }
+
+        public Persona (string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+        {
+
+        }
+
+        public Persona(string nombre, string apellido,string dni, ENacionalidad nacionalidad)
+        {
+
+        }
+
+        public string ToString()
+        {
+
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+            if (dato>0 && dato<=89999999)
+            {
+                nacionalidad = ENacionalidad.Argentino;
+            }
+            else if(dato>89999999 && dato<=99999999)
+            {
+                nacionalidad = ENacionalidad.Extranjero;
+            }
+            //else   {                exepcion            }
+
+            return dato;
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            int dni;
+            if(int.TryParse(dato, out dni)) //Si es número correcto retornará true y saldrá
+            {
+                
+            }
+            return dni;
+        }
+
+        private string ValidarNombreApellido(string dato)
+        {
+            int cantidad = dato.Length;
+            
+            for(int i=0; i<=cantidad; i++)
+            {
+                
+            }
+
+        }
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ TP-03/Clases_Abstractas/Universitario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Abstractas
+{
+    public abstract class Universitario : Persona
+    {
+        private int legajo;
+
+        public bool Equals(object obj)
+        {
+
+        }
+
+        protected string MostrarDatos() //* es protected?
+        {
+
+        }
+
+        public static bool operator ==(Universitario pg1, Universitario pg2)
+        {
+            bool retorno = false;
+            /*
+            foreach (Llamada call in c.listaDeLlamadas)
+            {
+                if (llamada == call)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+            */
+            return retorno;
+        }
+
+        public static bool operator !=(Universitario pg1, Universitario pg2)
+        {
+            return !(pg1 == pg2);
+        }
+
+        protected abstract string ParticiparEnClase()
+        {
+
+        }
+
+        public Universitario()
+        {
+
+        }
+
+        public Universitario(int legajo, string nombre, string apellido, string dni, ENacionalidad nacionalidad) :base(nombre, apellido, dni, nacionalidad)
+        {
+
+        }
+
+    }
+}
